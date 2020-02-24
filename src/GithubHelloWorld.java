778e7c87d1314dc88f4d7c5626ab6f0046c4050d
Antonio Forrest <antonio.forrest.2023@student.lhschools.org>
2020-02-24 13:01:01 -0500

    Called Hello World Method

--- src/GithubHelloWorld.java
+++ src/GithubHelloWorld.java
@@ -1,14 +1,16 @@
 public class GithubHelloWorld {
     public static void main(String[] args) {
-    System.out.println("Hello World");
-        
+        System.out.println("Hello World");
+        helloWorld();
     }
 
     public static void helloWorld(){
-        System.out.println("Hello World");
+
+        System.out.println("Hello Github World");
     }
 
 
 
 
 }
+
